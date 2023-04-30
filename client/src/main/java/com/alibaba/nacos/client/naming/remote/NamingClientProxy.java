01699afc30e6471896b634c227bf22c2101c6581
kangliang1 <wltjack@sina.com>
2023-04-30 13:49:42 +0800

    start read code

--- client/src/main/java/com/alibaba/nacos/client/naming/remote/NamingClientProxy.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/remote/NamingClientProxy.java
@@ -35,6 +35,7 @@ public interface NamingClientProxy extends Closeable {
     
     /**
      * Register a instance to service with specified instance properties.
+     * 注册服务实例
      *
      * @param serviceName name of service
      * @param groupName   group of service
